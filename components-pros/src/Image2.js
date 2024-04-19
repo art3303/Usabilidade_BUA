fc6c6814e09a5f5475ee826325acfe733627a191
Danilo-Miguel <dan.sm.mineiro@gmail.com>
2024-04-19 09:35:11 -0300

    Introdução sobre componentes e props

---
+++ components-pros/src/Image2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Image2 = () => {
+  return (
+    <img src="bird2.jpg" alt="Imagem 2" className="img-fluid" />
+  );
+}
+
+export default Image2
---
+++ components-pros/src/Image3.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Image3 = () => {
+  return (
+    <img src="bird3.jpg" alt="Imagem 1" className="img-fluid" />
+  );
+}
+
+export default Image3
