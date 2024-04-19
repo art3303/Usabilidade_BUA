1adfad3d0f142cb81699124eac7407db5999a618
Danilo-Miguel <dan.sm.mineiro@gmail.com>
2024-04-19 12:02:17 -0300

    Formulário de login com os componentes separados

---
+++ login-form-react-three-components/src/InputText.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css';
+
+function InputText(props){
+
+  return(
+   <div>
+    <label htmlFor={props.id}> {props.label}</label>
+    <input type={props.type} id={props.id} name={props.name} value={props.value} required/>
+    </div>
+  )
+
+}
+
+export default InputText
